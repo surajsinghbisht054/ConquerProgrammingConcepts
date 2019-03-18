7831dee931ee5f7d553d6c7353867511e3dcdd52
slowestwind <slowestwind@gmail.com>
2019-03-18 15:02:18 +0530

    New Update By Team Members

---
+++ LoginWithPhp/config.php
@@ -0,0 +1,7 @@
+<?php
+  define('DB_SERVER', 'localhost:3000');
+  define('DB_USERNAME', 'stunning-Aspire-ES1-531');
+  define('DB_PASSWORD', '');
+  define('DB_DATABASE', 'database');
+  $db = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_DATABASE);
+ ?>
